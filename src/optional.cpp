c3770dde45dd0e0821fa4c84f9f8044a339130cf
Daniel Bevenius <daniel.bevenius@gmail.com>
2024-03-17 07:46:20 +0100

    src: add more optional examples
    Signed-off-by: Daniel Bevenius <daniel.bevenius@gmail.com>

--- src/optional.cpp
+++ src/optional.cpp
@@ -20,4 +20,10 @@ int main() {
     //if (auto str = create(true)) {
         std::cout << "str = " << str.value() << std::endl;
     }
+
+    // Default constructor and using value_or:
+    std::optional<std::string> str;
+    std::cout << "str = " << str.value_or("Empty") << std::endl;
+    str.emplace("Emplaced");
+    std::cout << "str = " << str.value() << std::endl;
 }
