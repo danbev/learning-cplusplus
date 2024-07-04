390561e2c92455badb245b1856ec50f136f5b592
Daniel Bevenius <daniel.bevenius@gmail.com>
2024-07-04 07:58:31 +0200

    src: add example of omp single directive
    Signed-off-by: Daniel Bevenius <daniel.bevenius@gmail.com>

--- src/openmp.cpp
+++ src/openmp.cpp
@@ -8,6 +8,10 @@ int main() {
         int omp_id = omp_get_thread_num();
         pthread_t pthread_id = pthread_self();
         printf("OpenMP thread %d has POSIX thread ID %lu\n", omp_id, (unsigned long) pthread_id);
+        #pragma omp single
+        {
+            printf("This will only be printed once! %d\n", omp_id);
+        }
     }
 
     return 0;
