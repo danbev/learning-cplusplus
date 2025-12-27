9d1fc553b7cdcbab4fa323c3c1cfba3207ce26b2
Daniel Bevenius <daniel.bevenius@gmail.com>
2025-12-27 07:28:24 +0100

    src: add std::variant example

---
+++ src/variant.cpp
@@ -0,0 +1,28 @@
+#include <cstdio>
+#include <variant>
+#include <string>
+
+int main(int argc, char** argv) {
+    printf("std::variant example\n");
+    std::variant<int, float, std::string> v;
+
+    v = 10;
+    int i = std::get<int>(v);
+    printf("Integer value: %d\n", i);
+
+    try {
+        float f = std::get<float>(v);
+    } catch (const std::bad_variant_access& e) {
+        printf("Caught exception: %s\n", e.what());
+    }
+
+    v = 3.14f;
+    float f = std::get<float>(v);
+    printf("Float value: %f\n", f);
+                                  
+    v = "Hello World";
+    std::string s = std::get<std::string>(v);
+    printf("String value: %s\n", s.c_str());
+
+    return 0;
+}
