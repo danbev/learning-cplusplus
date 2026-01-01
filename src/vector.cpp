7a12ffd57262912acf3cd8e19c53ee39863f2d3c
Daniel Bevenius <daniel.bevenius@gmail.com>
2026-01-01 15:03:23 +0100

    src: minor clean up of vector example

--- src/vector.cpp
+++ src/vector.cpp
@@ -1,12 +1,11 @@
 #include <vector>
-#include <stdio.h>
+#include <cstdio>
 
 int main(int argc, char** argv) {
     fprintf(stdout, "Vector example\n");
 
     std::vector<int> v = {10, 20, 30, 40};
     auto it1 = v.begin() + 0;
-    auto it2 = v.begin();
-    printf("%d: \n", (it1 - it2));
+    printf("%d\n", *it1);
     return 0;
 }
