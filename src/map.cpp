342f8e9d5fd9fb0913264cd86507295e8c77a547
Daniel Bevenius <daniel.bevenius@gmail.com>
2026-01-16 07:03:04 +0100

    src: add multimap example in map.cpp

--- src/map.cpp
+++ src/map.cpp
@@ -6,19 +6,33 @@ int main(int argc, char** argv) {
     fprintf(stdout, "map example\n");
 
     std::map<int, int> m;
-    printf("m size: %d\n", m.size());
+    printf("m size: %ld\n", m.size());
     m[1] = 1;
 
     printf("m[1] = %d\n", m[1]);
     // Notice that m[inx] will insert a new element if it doesn't exist!
     printf("m[2] = %d\n", m[2]);
-    printf("m size: %d\n", m.size());
+    printf("m size: %ld\n", m.size());
 
     printf("m.at(1) = %d\n", m.at(1));
     try {
         printf("m.at(3) = %d\n", m.at(3));
     } catch (std::out_of_range& e) {
         printf("m.at(3) will throw an exeption if the value does not exist!\n");
+    }
+
+    printf("\n\nmultimap example\n");
+    std::multimap<int, std::string> multi_map;
+
+    multi_map.insert({1, "Dan"});
+    multi_map.insert({1, "Tilda"});
+
+    printf("multi_map size: %d\n", (int) multi_map.size());
+
+    auto range = multi_map.equal_range(1);
+    printf("Values for key 1 (# %ld): \n", multi_map.count(1));
+    for (auto it = range.first; it != range.second; ++it) {
+        printf("%s \n", it->second.c_str());
     }
 
     return 0;
